756b028565678a8164e9a1b69c0ded6b9d90bc6d
Victor Rodriguez <victormanuelrodriguez0899@gmail.com>
2025-09-22 13:10:15 -0600

    debug: add basic startup logs to main.tsx
    Add logs at the very beginning of app startup:
    - Log when main.tsx loads
    - Log before App component render
    - Log after render initiation
    
    This will verify if React is loading at all in production.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
+console.log('🚀 MARIO PARTY APP STARTING - main.tsx loaded');
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+
+console.log('🚀 About to render App component');
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+console.log('🚀 App component render initiated');
